bbcd1e9664e60460acadee593f06158d6f994fab
Alexandr Kravchenko <sasha121196@mail.ru>
2019-12-24 13:34:48 +0300

    Create lab03.py

---
+++ lab03/lab03.py
@@ -0,0 +1,38 @@
+from keras.datasets import mnist
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.utils import np_utils
+
+batch_size = 128
+num_epochs = 20
+hidden_size = 512
+
+num_train = 60000
+num_test = 10000
+
+height, width, depth = 28, 28, 1
+num_classes = 10
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(num_train, height * width)
+X_test = X_test.reshape(num_test, height * width)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+Y_train = np_utils.to_categorical(y_train, num_classes)
+Y_test = np_utils.to_categorical(y_test, num_classes)
+
+inp = Input(shape=(height * width,))
+hidden_1 = Dense(hidden_size, activation='relu')(inp)
+hidden_2 = Dense(hidden_size, activation='relu')(hidden_1)
+out = Dense(num_classes, activation='softmax')(hidden_2)
+
+model = Model(input=inp, output=out)
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, Y_train,  batch_size=batch_size, nb_epoch=num_epochs, verbose=1, validation_split=0.1) 
+model.evaluate(X_test, Y_test, verbose=1)  
